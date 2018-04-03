89d478876b63d601f6bb0745266421100f9942c9
Loquicom <Loquicom@users.noreply.github.com>
2018-04-03 12:30:56 +0200

    Intégration nouveaux composant CSS et JS
    Ajout padding-margin.css et message.js

--- system/setup_file/preset/webpage.php
+++ system/setup_file/preset/webpage.php
@@ -15,8 +15,10 @@ defined('FC_INI') or exit('Acces Denied');
         <link type="text/css" rel="stylesheet" href="<?= assets_url('css/material-icons.css') ?>"/>
         <!--Import materialize.css-->
         <link type="text/css" rel="stylesheet" href="<?= assets_url('css/materialize.css') ?>"  media="screen,projection"/>
-        <!--Import style.css -->
+        <!--Import style.css-->
         <link type="text/css" rel="stylesheet" href="<?= assets_url('css/style.css') ?>"  media="screen,projection"/>
+        <!--Import Other CSS-->
+        <link type="text/css" rel="stylesheet" href="<?= assets_url('css/padding-margin.css') ?>"  media="screen,projection"/>
 
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -30,8 +32,9 @@ defined('FC_INI') or exit('Acces Denied');
             <?= $body ?>
         </main>
 
-        <!--Import jQuery before materialize.js-->
+        <!--Import jQuery, materialize.js, and other JS-->
         <script type="text/javascript" src="<?= assets_url('js/jquery-3.1.2.min.js') ?>"></script>
         <script type="text/javascript" src="<?= assets_url('js/materialize.js') ?>"></script>
+        <script type="text/javascript" src="<?= assets_url('js/message.js') ?>"></script>
     </body>
 </html>
